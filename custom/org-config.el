3a86441492ce569ba15c1a7a690704b9e35f08aa
Jellu <105118442+Debilesque@users.noreply.github.com>
2024-02-16 14:05:46 -0600

    new setting retrieved from org-modern

--- custom/org-config.el
+++ custom/org-config.el
@@ -71,5 +71,42 @@
 ;; latex previews
 (plist-put org-format-latex-options :scale 2)
 
+                                        ; --- Modern ---
+;; Add frame borders and window dividers
+(modify-all-frames-parameters
+ '((right-divider-width . 1)
+   (internal-border-width . 1)))
+(dolist (face '(window-divider
+                window-divider-first-pixel
+                window-divider-last-pixel))
+  (face-spec-reset-face face)
+  (set-face-foreground face (face-attribute 'default :background)))
+(set-face-background 'fringe (face-attribute 'default :background))
+
+(setq
+ ;; Edit settings
+ org-auto-align-tags nil
+ org-tags-column 0
+ org-catch-invisible-edits 'show-and-error
+ org-special-ctrl-a/e t
+ org-insert-heading-respect-content t
+
+ ;; Org styling, hide markup etc.
+ org-hide-emphasis-markers t
+ org-pretty-entities t
+ org-ellipsis "…"
+
+ ;; Agenda styling
+ org-agenda-tags-column 0
+ org-agenda-block-separator ?─
+ org-agenda-time-grid
+ '((daily today require-timed)
+   (800 1000 1200 1400 1600 1800 2000)
+   " ┄┄┄┄┄ " "┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄")
+ org-agenda-current-time-string
+ "◀── now ─────────────────────────────────────────────────")
+
+(global-org-modern-mode)
+
 (provide 'org-config)
 ;;; org-config.el ends here
